005128db6bf511d7fb47aef7a7f1c55b1e1394fe
Benson Michael Owen <lordkz19@gmail.com>
2024-07-07 08:02:14 +0100

    DB .env created

--- backend/db.php
+++ backend/db.php
@@ -1,16 +1,19 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once dirname(__DIR__) . "/backend/exportENV.php";
+// require_once dirname(__DIR__) . "/backend/exportENV.php";
 
 
-// $server__DB_rays = $DB_HOST_RAYSON;
-// $user_DB_rays = $DB_USER_RAYSON;
-// $pass_DB_rays = $DB_PASS_RAYSON;
-// $db_DB_rays = $DB_NAME_RAYSON;
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-// require_once "exportENV.php";
+require_once __DIR__ .'/../vendor/autoload.php';// If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+// Inmport Environment Variables
+include __DIR__ .'/exportENV.php';
 
 $server__DB_rays = $_ENV['DB_HOST'];
 $user_DB_rays = $_ENV['DB_USER'];

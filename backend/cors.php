f6b5a415922bc504ec8d146895a930316f63987a
Benson Michael Owen <lordkz19@gmail.com>
2024-07-07 10:13:34 +0100

    Cors To Back

--- backend/cors.php
+++ backend/cors.php
@@ -1,13 +1,10 @@
 <?php
 // function enableCORS() {
     if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Allow from a specific origin
+        // Allow from any origin
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400'); // cache for 1 day
-    } else {
-        // Fallback if HTTP_ORIGIN is not set
-        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
 
     // Access-Control headers are received during OPTIONS requests
@@ -16,10 +13,8 @@
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         }
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
         }
         exit(0);
     }
 // }
-
-// enableCORS();

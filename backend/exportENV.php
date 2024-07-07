4e1358c4dde4947bcedb3c64499ffeb250ddfa17
Benson Michael Owen <lordkz19@gmail.com>
2024-07-07 08:13:03 +0100

    Final Corrections for Environment Variables

--- backend/exportENV.php
+++ backend/exportENV.php
@@ -4,7 +4,6 @@ require_once __DIR__ .'/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 if($dotenv->load()){
-echo "Environment Variable";
 }else{
     echo "Could Not Load Environment variables";
 }

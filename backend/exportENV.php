4871f1e86a434e6357719850c023badf57b46709
Benson Michael Owen <lordkz19@gmail.com>
2024-07-07 07:55:17 +0100

    Environment Variables

--- backend/exportENV.php
+++ backend/exportENV.php
@@ -2,6 +2,5 @@
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
-// Adjust the path to point to the backend directory
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../backend');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();

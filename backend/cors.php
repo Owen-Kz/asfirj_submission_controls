53977ac778d5fbf8e27fd155a5918c0d57224e5a
Benson Michael Owen <lordkz19@gmail.com>
2024-07-07 10:01:16 +0100

    CORS

--- backend/cors.php
+++ backend/cors.php
@@ -1,25 +1,25 @@
 <?php
 // function enableCORS() {
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Allow from a specific origin
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400'); // cache for 1 day
-    } else {
-        // Fallback if HTTP_ORIGIN is not set
-        header("Access-Control-Allow-Origin: *");
-    }
+    // if (isset($_SERVER['HTTP_ORIGIN'])) {
+    //     // Allow from a specific origin
+    //     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    //     header('Access-Control-Allow-Credentials: true');
+    //     header('Access-Control-Max-Age: 86400'); // cache for 1 day
+    // } else {
+    //     // Fallback if HTTP_ORIGIN is not set
+    //     header("Access-Control-Allow-Origin: *");
+    // }
 
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        }
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-        }
-        exit(0);
-    }
+    // // Access-Control headers are received during OPTIONS requests
+    // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+    //         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    //     }
+    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+    //         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    //     }
+    //     exit(0);
+    // }
 // }
 
 // enableCORS();

7e3d9bc2ccdeaf1877ac3bb2023eaff4eab3e63c
Benson Michael Owen <lordkz19@gmail.com>
2024-07-10 18:03:39 +0100

    Count function added for ediors

---
+++ backend/editors/countReviewed.php
@@ -0,0 +1,32 @@
+<?php
+
+include "../cors.php";
+include "../db.php";
+include "./isAdminAccount.php";
+$userId = $_GET["u_id"];
+
+if(isset($userId)){
+    // if(isAdminAccount($userId)){
+        $stmt = $con->prepare("SELECT COUNT(*) AS `count` FROM `submissions` WHERE `status` = 'review_submitted' ");
+        // $stmt->bind_param("s", $userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $count = $row["count"];
+        $response = array("success"=>"CountSuccess", "count"=> $count);
+        echo json_encode($response);
+    // }else{
+    //     $stmt = $con->prepare("SELECT COUNT(*) AS `count` FROM `submitted_for_edit` WHERE md5(`editor_email`) = ? AND `status` = 'edit_invitation_accepted'");
+    //     $stmt->bind_param("s", $userId);
+    //     $stmt->execute();
+    //     $result = $stmt->get_result();
+    //     $row = $result->fetch_assoc();
+    //     $count = $row["count"];
+    //     $response = array("success"=>"CountSuccess", "count"=> $count);
+    //     echo json_encode($response);
+    // }
+   
+
+}else{
+    echo json_encode(array("error"=>"Invalid Parameters"));
+}

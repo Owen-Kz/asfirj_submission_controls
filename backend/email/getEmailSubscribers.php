b1c5aec0fedc9cfcbe743f55551aa414c3cbce10
Benson Michael Owen <lordkz19@gmail.com>
2024-08-05 15:17:21 +0100

    Get News letter subscribers

---
+++ backend/email/getEmailSubscribers.php
@@ -0,0 +1,29 @@
+<?php 
+include "../../cors.php";
+include "../../db.php";
+include "../isAdministrator.php";
+
+$adminId = $_GET["a_id"];
+if(isset($adminId)){
+    if(isAdministrator($adminId)){
+        $stmt = $con->prepare("SELECT * FROM `news_letter_subscribers` WHERE 1 ORDER BY `id` DESC");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows > 0){
+            $emailList = array();
+            while($row = $result->fetch_assoc()){
+                $emailList[] = $row;
+            }
+            echo json_encode(array("success"=>"emailListFound", "emailList" => $emailList));
+        }else{
+            echo json_encode(array("error"=>"NoEmailListFound", "emailList" => []));
+
+        }
+    }else{
+        echo json_encode(array("error"=>"unAuthorizedAccess", "emailList" => []));
+        
+    }
+}else{
+    echo json_encode(array("error"=>"invalidParameters", "emailList" => []));
+    
+}

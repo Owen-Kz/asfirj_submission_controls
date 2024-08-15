416eb75008c5ce20d33aef9b5e73c9f334430141
Benson Michael Owen <lordkz19@gmail.com>
2024-08-15 17:36:20 +0100

    Account Migration succesfuly

---
+++ backend/editors/migrateAccount.php
@@ -0,0 +1,67 @@
+<?php
+
+include "../cors.php";
+include "../db.php";
+include "./isAdminAccount.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$authorEmail = $data["id"];
+$admin = $data["admin"];
+
+if(isAdminAccount($admin) && $authorEmail != ''){
+    // Find profile Details 
+    $stmt = $con->prepare("SELECT * FROM `authors_account` WHERE `email` =  ?");
+    $stmt->bind_param("s", $authorEmail);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        $password = $row["password"];
+        $email = $row["email"];
+        $fullname = $row["prefix"]. " " .$row["firstname"]. " ". $row["lastname"]." ".$row["othername"];
+        $editorial_level = "sectional_editor";
+
+        $stmt = $con->prepare("SELECT * FROM `editors` WHERE `email` =?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows > 0){
+            $response = array("error" => "This User is already an Editor");
+        }else{
+            $stmt = $con->prepare("INSERT INTO `editors`(`email`,`fullname`, `password`, `editorial_level`) VALUES(?,?,?,?)");
+            if(!$stmt){
+             $response = array("error"=>"Could Not prepare statement $stmt->error");
+             exit ;
+            }
+            $stmt->bind_param("ssss", $email, $fullname, $password, $editorial_level);
+            if($stmt->execute()){
+                $yes = "yes";
+                
+                $stmt = $con->prepare("UPDATE `authors_account` SET `is_editor` = ?, `is_available_for_review` =?, `is_reviewer` = ? WHERE `email` = ?");
+                if(!$stmt){
+                    $response = array("error"=>"Could Not prepare statement $stmt->error");
+                    exit ;
+                   }
+                $stmt->bind_param("ssss", $yes, $yes, $yes, $email);
+                if($stmt->execute()){
+             $response = array("success"=>"Account Migration Successful");
+                }else{
+             $response = array("error"=>"Could Not UPdate Account");
+
+                }
+                
+            }else{
+             $response = array("error"=>"Could Not Create editor");
+
+            }
+        }
+    }else{
+
+    }
+}else{
+    $response = array("error"=>"Could Not Delete Account, You are not an Admin $data");
+}
+
+echo json_encode($response);

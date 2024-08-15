74e16c075ab9264202e0123ec5953436e7acad4b
Benson Michael Owen <lordkz19@gmail.com>
2024-08-15 19:18:51 +0100

    Login function

--- backend/accounts/login.php
+++ backend/accounts/login.php
@@ -36,7 +36,7 @@ if(isset($pass) && isset($email_post)){
    
 	//if user record is available in database then $count will be equal to 1
 
-	if($count == 1){
+	if($result){
         // Get and verify the users password if the account exists  
         $row = mysqli_fetch_array($run_query);
         $storedHashedPassword = $row["password"];

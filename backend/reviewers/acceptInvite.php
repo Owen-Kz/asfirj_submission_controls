ce45800f31fcabcbc1b35c4edcc97c0fb05288dd
Benson Michael Owen <lordkz19@gmail.com>
2025-01-10 04:05:06 +0100

    Invitaion accpeted set to single row data

--- backend/reviewers/acceptInvite.php
+++ backend/reviewers/acceptInvite.php
@@ -12,7 +12,7 @@ $revision_id = $data["articleId"];
 $reviewerEmail = $data["reviewerEmail"];
 
 // Find Details of the Article 
-$stmt = $con->prepare("SELECT * FROM `submitted_for_review` WHERE `article_id` = ? AND `reviewer_email` =?");
+$stmt = $con->prepare("SELECT * FROM `submitted_for_review` WHERE `article_id` = ? AND `reviewer_email` =? AND `invitation_status` != 'review_submitted' ");
 $stmt->bind_param("ss", $revision_id, $reviewerEmail);
 if($stmt->execute()){
     $result = $stmt->get_result();
@@ -20,11 +20,11 @@ if($stmt->execute()){
     if($count > 0){
         $row = mysqli_fetch_array($result);
         $editor_email = $row["submitted_by"];
-        $stmt = $con->prepare("UPDATE `submissions` SET `status` = 'review_request_accepted'  WHERE `revision_id` = ?");
+        $stmt = $con->prepare("UPDATE `submissions` SET `status` = 'review_request_accepted'  WHERE `revision_id` = ? ");
         $stmt->bind_param("s",$revision_id);
         if($stmt->execute()){
-            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_request_accepted'  WHERE `article_id` = ?");
-            $stmt->bind_param("s",$revision_id);
+            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_request_accepted'  WHERE `article_id` = ? AND `reviewer_email` = ?");
+            $stmt->bind_param("ss",$revision_id, $reviewerEmail);
             $stmt->execute();
             ReviewConfirmationEmail($editor_email, $reviewerEmail, "accepted");
             $response = array("status"=>"success", "message"=>"Review Request has been accepted");
--- backend/reviewers/rejectInvite.php
+++ backend/reviewers/rejectInvite.php
@@ -12,7 +12,7 @@ $revision_id = $data["articleId"];
 $reviewerEmail = $data["reviewerEmail"];
 
 // Find Details of the Article 
-$stmt = $con->prepare("SELECT * FROM `submitted_for_review` WHERE `article_id` = ? AND `reviewer_email` =?");
+$stmt = $con->prepare("SELECT * FROM `submitted_for_review` WHERE `article_id` = ? AND `reviewer_email` =? AND `invitation_status` != 'review_submitted'");
 $stmt->bind_param("ss", $revision_id, $reviewerEmail);
 if($stmt->execute()){
     $result = $stmt->get_result();
@@ -23,8 +23,8 @@ if($stmt->execute()){
         $stmt = $con->prepare("UPDATE `submissions` SET `status` = 'review_request_rejected'  WHERE `revision_id` = ?");
         $stmt->bind_param("s",$revision_id);
         if($stmt->execute()){
-            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_request_rejected'  WHERE `article_id` = ?");
-            $stmt->bind_param("s",$revision_id);
+            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_request_rejected'  WHERE `article_id` = ? AND `reviewer_email` = ?");
+            $stmt->bind_param("ss",$revision_id, $reviewerEmail);
             $stmt->execute();
             ReviewConfirmationEmail($editor_email, $reviewerEmail, "rejected");
             $response = array("status"=>"success", "message"=>"Review Request has been rejected");
@@ -32,7 +32,7 @@ if($stmt->execute()){
         }
        
     }else{
-        $response = array("status"=>"error", "message"=>"Could not find Article");
+        $response = array("status"=>"error", "message"=>"Invalid Action: Invitation accepted or the review has been submitted");
         echo json_encode($response);
     }
 }else{

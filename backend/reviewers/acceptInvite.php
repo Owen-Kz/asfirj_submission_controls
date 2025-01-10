c12e867bdc6c34ad51f9a7bb5e6834a48d751f9e
Benson Michael Owen <lordkz19@gmail.com>
2025-01-10 04:19:41 +0100

    Accept reviewer invitations status updated

--- backend/reviewers/acceptInvite.php
+++ backend/reviewers/acceptInvite.php
@@ -20,10 +20,10 @@ if($stmt->execute()){
     if($count > 0){
         $row = mysqli_fetch_array($result);
         $editor_email = $row["submitted_by"];
-        $stmt = $con->prepare("UPDATE `submissions` SET `status` = 'review_request_accepted'  WHERE `revision_id` = ? ");
+        $stmt = $con->prepare("UPDATE `submissions` SET `status` = 'review_invitation_accepted'  WHERE `revision_id` = ? ");
         $stmt->bind_param("s",$revision_id);
         if($stmt->execute()){
-            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_request_accepted'  WHERE `article_id` = ? AND `reviewer_email` = ?");
+            $stmt = $con->prepare("UPDATE `submitted_for_review` SET `status` = 'review_invitation_accepted'  WHERE `article_id` = ? AND `reviewer_email` = ?");
             $stmt->bind_param("ss",$revision_id, $reviewerEmail);
             $stmt->execute();
             ReviewConfirmationEmail($editor_email, $reviewerEmail, "accepted");

691fa08e75a60652d0ae54d768644d2015b47f1b
Benson Michael Owen <lordkz19@gmail.com>
2025-02-05 21:56:38 +0100

    Archive submissions added

---
+++ backend/editors/allAcceptedSubmissions.php
@@ -0,0 +1,48 @@
+<?php
+
+include "../cors.php";
+include "../db.php";
+include "./isAdminAccount.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+$adminId = $data["admin_id"];
+if (isset($adminId)) {
+    $isAdminAccount = isAdminAccount($adminId);
+    if ($isAdminAccount) {
+        $stmt = $con->prepare("SELECT * FROM `submissions` WHERE `status` = 'accepted' ORDER BY `id` DESC");
+        if (!$stmt) {
+            echo json_encode(array("error" => $stmt->error));
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $submissions = array();
+
+        while ($row = $result->fetch_assoc()) {
+            foreach ($row as $key => $value) {
+                // Check if $value is not null
+                if ($value !== null) {
+                    $row[$key] = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+                } else {
+                    // You can also set a default value or keep it as null if needed
+                    $row[$key] = ''; // or null
+                }
+            }
+            $submissions[] = $row;
+        }
+
+
+        // var_dump($submissions);
+
+        $json = json_encode(array("success" => "Admin Account", "submissions" => $submissions));
+        if ($json === false) {
+            echo json_last_error_msg();
+        } else {
+            echo $json;
+        }
+
+    } else {
+        echo json_encode(array("error" => "Not Admin Account"));
+    }
+} else {
+    echo json_encode(array("error" => "Invalid Parameters"));
+}
---
+++ backend/editors/archivePaper.php
@@ -0,0 +1,35 @@
+<?php
+
+include "../cors.php";
+include "../db.php";
+include "./isAdminAccount.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+$submissinoId = $data["submissionId"];
+
+if(isset($submissinoId)){
+    $stmt = $con->prepare("SELECT * FROM `submissions` WHERE `revision_id` = ?");
+    $stmt->bind_param("s", $submissinoId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+if($result->num_rows > 0){
+    $mainArticleId = $result->fetch_assoc()["article_id"];
+    $stmt = $con->prepare("INSERT INTO archived_submissions
+     SELECT * FROM submissions WHERE article_id = ?");
+     $stmt->bind_param("s", $mainArticleId);
+        if($stmt->execute()){
+        $stmt = $con->prepare("DELETE FROM submissions WHERE article_id = ?");
+        $stmt->bind_param("s", $mainArticleId);
+        $stmt->execute();
+        }else{
+            echo json_encode(array("error" => "Submission not Archived"));
+        }
+        echo json_encode(array("success" => "Submission Archived"));
+}else{
+    echo json_encode(array("error" => "Submission ID not found"));
+}
+
+  
+} else {
+    echo json_encode(array("error" => "Submission ID is not SET"));
+}

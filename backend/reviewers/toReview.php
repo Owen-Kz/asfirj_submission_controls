84ec0274a7e10c16d9b164afd0136678b9c597f2
Benson Michael Owen <lordkz19@gmail.com>
2025-02-19 20:06:40 +0100

    Reviewer dashboard updated

--- backend/reviewers/toReview.php
+++ backend/reviewers/toReview.php
@@ -7,9 +7,18 @@ include "../db.php";
 
 // $email = $data["encrypted"];
 $email = $_GET["user"];
+$userEMail = "";
+$stmt = $con->prepare("SELECT * FROM authors_account WHERE md5(id) = ? OR md5(email) = ? OR email = ?", );
+$stmt->bind_param("sss", $email, $email, $email);
+$stmt->execute();
+$results = $stmt->get_result();
+if(mysqli_num_rows($results) > 0){
+    $row = $results->fetch_assoc();
+    $userEMail = $row["email"];
+
 
 $stmt = $con->prepare("SELECT * FROM `submitted_for_review` WHERE `reviewer_email` = ? AND `status` = 'review_invitation_accepted' ORDER BY `id` DESC");
-$stmt->bind_param("s", $email);
+$stmt->bind_param("s", $userEMail);
 $stmt->execute();
 $result = $stmt->get_result();
 if(mysqli_num_rows($result) > 0){
@@ -74,4 +83,8 @@ if(mysqli_num_rows($result) > 0){
     echo "<tr><td>You have no new review requests";
     // $response = array("status" => "success", "submissionsToReview" => []);
     // echo json_encode($response);
+}
+}else{
+    echo "<tr><td>Internal Server Error";
+
 }
